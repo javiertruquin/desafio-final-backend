3ef8550613ce0ca3f76f56efba4a0aeb741e46fb
javier Truquin <javiertruquin@gmail.com>
2021-05-13 18:06:09 -0300

    backend base

---
+++ controllers/usuarioController.js
@@ -0,0 +1,40 @@
+const Usuario = require("../models/Usuario");
+const bcryptjs = require("bcryptjs");
+const { validationResult } = require("express-validator");
+
+exports.crearUsuario = async (req, res) => {
+    // revisamos los errores
+    const errores = validationResult(req);
+    if (!errores.isEmpty()) {
+        return res.status(400).json({ msg: errores.array() });
+    }
+
+    let { email, password } = req.body;
+
+    try {
+        let usuarioEncontrado = await Usuario.findOne({ email });
+        if (usuarioEncontrado) {
+            return res.status(400).send("Email ya esta en uso");
+        }
+
+        //nuevo usuario
+        let usuario = new Usuario(req.body);
+
+        //hashear el password
+        const salt = await bcryptjs.genSalt(10);
+        usuario.password = await bcryptjs.hash(password, salt);
+
+        //guardar usuario
+        await usuario.save();
+
+        //mensaje de exito
+        res.send("Usuario Creado Correctamente");
+    } catch (error) {
+        console.log(error);
+        res.status(400).send("Hubo un error al crear el Usuario");
+    }
+};
+
+exports.obtenerUsuarios = (req, res) => {
+    console.log("funcion obtener usuarios");
+};
