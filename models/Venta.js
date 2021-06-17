963822507dfa7bfe3ec1cfd9a0fec11ebcab261d
Andres <andresj_077@hotmail.com>
2021-06-17 14:01:42 -0300

    modelo de "venta" agregado en mongo

--- models/Venta.js
+++ models/Venta.js
@@ -1,9 +1,31 @@
 const mongoose = require('mongoose');
 const VentaSchema = mongoose.Schema({
-    nombre: {
+    usuario: {
         type: String,
         required: true,
         trim: true,
+    },
+    domicilio: [
+        {
+            provincia: { type: String, trim: true },
+            localidad: { type: String, trim: true },
+            calle: { type: String, trim: true },
+            numero: { type: Number, trim: true },
+            piso: { type: String, trim: true },
+            telefono: { type: Number, trim: true },
+            observaciones: { type: String, trim: true },
+        },
+    ],
+    numeroventa: {
+        type: Number,
+        required: true,
+        trim: true,
+        default: 1,
+    },
+
+    fecha: {
+        type: Date,
+        default: Date.now(),
     },
 });
 
--- routes/ventaRoute.js
+++ routes/ventaRoute.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const ventaController = require('../controllers/ventaController');
 const { check } = require('express-validator');
 
-router.post('/', [check('nombre', 'El nombre es obligatorio').not().isEmpty()], ventaController.obtenerVenta);
+router.post(
+    '/',
+    [check('domicilio', 'El domicilio es obligatorio').not().isEmpty()],
+    ventaController.obtenerVenta
+);
 
 module.exports = router;
